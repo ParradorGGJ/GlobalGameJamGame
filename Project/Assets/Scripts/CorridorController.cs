8f6ba1f6bea31960c66cd3940e4daa7761ad40d7
jtabG <jtabgazzola@gmail.com>
2015-01-23 22:35:43 -0500

    Basic Corridor blocked out

---
+++ Project/Assets/Scripts/CorridorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CorridorController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_TransitionPoints = null;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public GameObject GetRandomTransitionPoint()
+    {
+        if (m_TransitionPoints == null) { return null; }
+
+        int randomRoom;
+        randomRoom = Random.Range(0, m_TransitionPoints.Length);
+
+        return m_TransitionPoints[randomRoom];
+    }
+
+}
