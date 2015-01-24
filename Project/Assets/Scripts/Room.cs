8f1560e789e904f8e137c44b2049f1810a560d35
jtabG <jtabgazzola@gmail.com>
2015-01-23 22:23:52 -0500

    Basic room class blocked out
    • Wrote very basic room class
    • Updated RoomAndCorridor visio

---
+++ Project/Assets/Scripts/Room.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField]
+    private int m_UniqueID;
+
+    [SerializeField]
+    private GameObject[] m_RoomObjects;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public GameObject[] GetRoomObjects()
+    {
+        return m_RoomObjects;
+    }
+
+    public int uniqueID
+    {
+        get { return m_UniqueID; }
+    }
+}
