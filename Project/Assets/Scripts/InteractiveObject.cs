e8483fa5874afc167520b9dde2bbd6c49e3da571
jtabG <jtabgazzola@gmail.com>
2015-01-23 23:27:36 -0500

    Created class for interactive objects
    • Class handles simple interaction with state updating. More proof of
    concept for when receiving updated state information through the network

---
+++ Project/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Parrador
+{
+
+    public class InteractiveObject : MonoBehaviour
+    {
+        [SerializeField]
+        private bool m_Used = false;
+
+        [SerializeField]
+        private Transform m_UsedState = null;
+
+        [SerializeField]
+        private Transform m_NormalState = null;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateState();
+        }
+
+        public void UpdateState()
+        {
+            if (m_Used)
+            {
+                transform.position = m_UsedState.position;
+                transform.rotation = m_UsedState.rotation;
+            }
+            else
+            {
+                transform.position = m_NormalState.position;
+                transform.rotation = m_NormalState.rotation;
+            }
+
+        }
+
+    }
+}
