d4e8292d35198ffc1da2920a30101990a9f094ca
jtabG <jtabgazzola@gmail.com>
2015-01-24 21:10:33 -0500

    Updated the character and the WasteBasketBehaviour and the RoomObject_Basket.prefab

--- Project/Assets/Scripts/RoomObjects/WasteBasketBehaviour.cs
+++ Project/Assets/Scripts/RoomObjects/WasteBasketBehaviour.cs
@@ -4,52 +4,79 @@ using System.Collections;
 
 namespace Parrador
 {
+    [RequireComponent(typeof (Rigidbody))]
     public class WasteBasketBehaviour : GenericObjectBehaviour
     {
 
         [SerializeField]
-        private Transform m_UsedStateTransform = null;
+        private Transform m_CarryAnchorPoint = null;
 
-        [SerializeField]
-        private Transform m_NormalStateTransform = null;
+        private Rigidbody m_RigidBody = null;
 
         // Use this for initialization
         void Start()
         {
             objectType = ObjectType.WasteBasket;
-            if ( (m_UsedStateTransform == null || m_NormalStateTransform == null) )
-            {
-                Debug.Log(objectType + " ObjectType not set up properly. ID: " + GetInstanceID());
-            }
+
+            m_RigidBody = GetComponent<Rigidbody>();
+            //if ( (m_UsedStateTransform == null || m_NormalStateTransform == null) )
+            //{
+            //    Debug.Log(objectType + " ObjectType not set up properly. ID: " + GetInstanceID());
+            //}
         }
 
         // Update is called once per frame
         void Update()
         {
-            UpdateState();  //only here while testing, will be called through interfact externally
+            if (m_CarryAnchorPoint != null)
+            {
+                UpdateObjectPositionRotation();
+            }
         }
 
 
         private void UpdateObjectPositionRotation()
         {
-            if (m_UsedStateTransform == null || m_NormalStateTransform == null) { return; }
-            
-            if (used)
-            {
-                transform.position = m_UsedStateTransform.position;
-                transform.rotation = m_UsedStateTransform.rotation;
-            }
-            else
-            {
-                transform.position = m_NormalStateTransform.position;
-                transform.rotation = m_NormalStateTransform.rotation;
-            }
+            transform.position = Vector3.Lerp(transform.position, m_CarryAnchorPoint.position, Time.deltaTime * 7.0f);
+            transform.rotation = m_CarryAnchorPoint.rotation;
         }
 
         public override void ObjectSpecificStateUpate()
         {
-            UpdateObjectPositionRotation();
+            if (used == true)
+            {
+                m_RigidBody.useGravity = false;
+            }
+            else
+            {
+                m_CarryAnchorPoint = null;
+                m_RigidBody.useGravity = true;
+            }
         }
 
+        void OnTriggerStay(Collider collider)
+        {
+            if (collider.CompareTag("Player") == false) { return; }
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                used = !used;
+                Debug.Log("E pressed by player, used state = " + used);
+                if (used == true)
+                {
+                    m_CarryAnchorPoint = collider.gameObject.transform.FindChild("CarryAnchor");
+                    if (m_CarryAnchorPoint == null) { Debug.Log("anchor not found"); }
+                    m_RigidBody.useGravity = false;
+                }
+                else
+                {
+                    m_CarryAnchorPoint = null;
+                    m_RigidBody.useGravity = true;
+                }
+
+                UpdateState();
+
+            }
+        }
     }
 }
