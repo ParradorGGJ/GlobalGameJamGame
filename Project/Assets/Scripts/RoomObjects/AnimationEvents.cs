462f3a9376149957284afe0c6655343a373e76fc
jtabG <jtabgazzola@gmail.com>
2015-01-25 00:29:37 -0500

    updated animation info for rocking chair using events to play creak sound made event script to hold animation event functions

---
+++ Project/Assets/Scripts/RoomObjects/AnimationEvents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Parrador
+{
+    /// <summary>
+    /// add more and more events for animation
+    /// </summary>
+    public class AnimationEvents : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource m_AudioSource = null;
+
+        void Start()
+        {
+            m_AudioSource = GetComponent<AudioSource>();
+        }
+
+        public void CreakEvent()
+        {
+            m_AudioSource.Play();
+        }
+    }
+}
