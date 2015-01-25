e4edc4b4a0f72086908d1bee8bb8bd1eb47980b0
Connie Fu <fu000032@algonquinlive.com>
2015-01-25 15:00:09 -0500

    Added general purpose sound playing script PlaySoundTrigger.cs

---
+++ Project/Assets/Scripts/PlaySoundTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource m_AudioSource = null;
+
+    [SerializeField]
+    private AudioClip m_OnUseClip = null;
+
+    [SerializeField]
+    private AudioClip m_OnEnterClip = null;
+
+    [SerializeField]
+    private bool m_PlayOnUse = false;
+
+    [SerializeField]
+    private bool m_PlayOnEnter = false;
+
+    void OnTriggerStay(Collider aCollider)
+    {
+        if (aCollider.CompareTag("Player") == false) { return; }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (m_AudioSource == null || m_OnUseClip == null) { return; }
+
+            m_AudioSource.PlayOneShot(m_OnUseClip);
+        }
+    }
+
+    void OnTriggerEnter(Collider aCollider)
+    {
+        if (aCollider.CompareTag("Player") == false) { return; }
+
+        if (m_AudioSource == null || m_OnUseClip == null) { return; }
+
+        m_AudioSource.PlayOneShot(m_OnEnterClip);
+    }
+}
